728e0cb4127324649181c845867b3b5a8d9ae944
rAnto <dark_neo3000@mail.ru>
2025-03-29 14:42:13 +0300

    Update admin for model User(inlines)

--- zaharovo/accounts/admin.py
+++ zaharovo/accounts/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from .models import Profile, Address
 
-# Register your models here.
+
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete = False
+    verbose_name_plural = 'Профили'
+
+
+class CustomUserAdmin(admin.ModelAdmin):
+    inlines = (ProfileInline,)
+
+
+admin.site.unregister(get_user_model())
+admin.site.register(get_user_model(), CustomUserAdmin)
+admin.site.register(Address)
